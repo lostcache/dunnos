85e48500012fd65375e5548d9c2394fa55ff5707
lostcache <harshdevisha9111@gmail.com>
2026-07-23 02:10:00 -0700

    WIP(uart): send impl

--- src/uart.rs
+++ src/uart.rs
@@ -19,6 +19,31 @@ fn set_baude_rate() {
     write_reg(DIVISOR_LATCH_MS_ADDR, 0);
 }
 
+fn send(val: u8) {
+    set_line_control_register_for_transmission();
+    write_reg(TRANSMITTER_HOLDING_REGISTER_ADDR, val);
+}
+
+fn set_line_control_register_for_transmission() {
+    const CHAR_LEN: u8 = (1 << 0) | (1 << 1); // 8 bit char
+    const NUM_STOP_BITS: u8 = 0 << 2; // 0->1 and 1->2 stop bits, receiver only uses first.
+    const PARITY_BIT: u8 = 1 << 3;
+    const EVEN_PARITY_SELECT: u8 = 0 << 4;
+    const STICK_PARITY: u8 = 0 << 5; // not needed
+    const BREAK_CONTROL_BIT: u8 = 0 << 6;
+    const DIVISOR_LATCH_ACCESS: u8 = 0 << 7;
+    write_reg(
+        LINE_CONTROL_REGISTER_ADDR,
+        CHAR_LEN
+            | NUM_STOP_BITS
+            | PARITY_BIT
+            | EVEN_PARITY_SELECT
+            | STICK_PARITY
+            | BREAK_CONTROL_BIT
+            | DIVISOR_LATCH_ACCESS,
+    );
+}
+
 fn set_DLAB() {
     write_reg(LINE_CONTROL_REGISTER_ADDR, 1 << 7);
 }
