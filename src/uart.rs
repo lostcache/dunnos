cc46cbfea1dcfe0211f181a1299448e80f4acae7
lostcache <harshdevisha9111@gmail.com>
2026-07-23 02:06:31 -0700

    feat(uart): set baude rate

--- src/uart.rs
+++ src/uart.rs
@@ -1,20 +1,30 @@
-const UART0: u32 = 0x10000000;
+// qemu specific address
+const UART0: u32 = 0x1000_0000;
 
 // the UART control registers.
-// some have different meanings for read vs write.
 // see http://byterunner.com/16550.html
-const RHR: u32 = 0; // receive holding register (for input bytes)
-const THR: u32 = 0; // transmit holding register (for output bytes)
-const IER: u32 = 1; // interrupt enable register
-const IER_RX_ENABLE: u32 = 1 << 0; // receiver interrupts
-const IER_TX_ENABLE: u32 = 1 << 1; // transmit interrupts
-const FCR: u32 = 2; // FIFO control register
-const FCR_FIFO_ENABLE: u32 = 1 << 0;
-const FCR_FIFO_CLEAR: u32 = 3 << 1; // clear the content of the two FIFOs
-const ISR: u32 = 2; // interrupt status register
-const LCR: u32 = 3; // line control register
-const LCR_EIGHT_BITS: u32 = 3 << 0;
-const LCR_BAUD_LATCH: u32 = 1 << 7; // special mode to set baud rate
-const LSR: u32 = 5; // line status register
-const LSR_RX_READY: u32 = 1 << 0; // input is waiting to be read from RHR
-const LSR_TX_IDLE: u32 = 1 << 5; // THR can accept another character to send
+const TRANSMITTER_HOLDING_REGISTER_ADDR: u8 = 0; // DLAB = 0
+const RECEIVER_BUFFER_REGISTER_ADDR: u8 = TRANSMITTER_HOLDING_REGISTER_ADDR; // DLAB = 0
+const LINE_CONTROL_REGISTER_ADDR: u8 = 3;
+const DIVISOR_LATCH_LS_ADDR: u8 = 0; // DLAB = 1
+const DIVISOR_LATCH_MS_ADDR: u8 = 1; // DLAB = 1
+
+pub(crate) fn init() {
+    set_baude_rate();
+}
+
+fn set_baude_rate() {
+    set_DLAB();
+    write_reg(DIVISOR_LATCH_LS_ADDR, 3);
+    write_reg(DIVISOR_LATCH_MS_ADDR, 0);
+}
+
+fn set_DLAB() {
+    write_reg(LINE_CONTROL_REGISTER_ADDR, 1 << 7);
+}
+
+fn write_reg(reg_addr: u8, val: u8) {
+    unsafe {
+        core::ptr::write_volatile((UART0 + reg_addr as u32) as *mut u8, val);
+    }
+}
